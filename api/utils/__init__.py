a372860ac5bc858d60d9f1e92ff3e433c31444b2
sunfkny <30853461+sunfkny@users.noreply.github.com>
2024-10-30 23:28:30 +0800

    Remove api

--- api/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .request import request_async
-from .timezone import get_tzinfo
-
-__all__ = ["request_async", "get_tzinfo"]
--- api/utils/timezone.py
+++
@@ -1,5 +0,0 @@
-import arrow
-
-
-def get_tzinfo(timezone: int):
-    return arrow.get(tzinfo=f"UTC{timezone:+03d}").tzinfo
